dde1f5707b408e84bebd9fd633d897db3e7d8142
Abhishek Pratap <35282546+AbhishekPratap05@users.noreply.github.com>
2021-04-15 02:57:35 +0530

    created footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FooterContainer, FooterSubHeading, FooterSubscription, FooterSubText } from './Footer.elements';
+
+const Footer = () => {
+	return (
+		<FooterContainer>
+			<FooterSubscription>
+				<FooterSubHeading>Get membership to receive the latest news and trends</FooterSubHeading>
+				<FooterSubText>You can unsubscribe at any time.</FooterSubText>
+			</FooterSubscription>
+		</FooterContainer>
+	);
+};
+
+export default Footer;
