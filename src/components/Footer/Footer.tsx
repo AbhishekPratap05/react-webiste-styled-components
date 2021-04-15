8f328e0f766696f3f1e09efc5550d19c5def53f8
Abhishek Pratap <35282546+AbhishekPratap05@users.noreply.github.com>
2021-04-15 10:52:58 +0530

    added subscribe form

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { FooterContainer, FooterSubHeading, FooterSubscription, FooterSubText } from './Footer.elements';
+import { Button } from '../../globalStyle';
+import {
+	FooterContainer,
+	FooterSubHeading,
+	FooterSubscription,
+	FooterSubText,
+	Form,
+	FormInput
+} from './Footer.elements';
 
 const Footer = () => {
 	return (
@@ -7,6 +15,10 @@ const Footer = () => {
 			<FooterSubscription>
 				<FooterSubHeading>Get membership to receive the latest news and trends</FooterSubHeading>
 				<FooterSubText>You can unsubscribe at any time.</FooterSubText>
+				<Form>
+					<FormInput name="email" type="email" placeholder="yor email" />
+					<Button fontBig>Subscribe</Button>
+				</Form>
 			</FooterSubscription>
 		</FooterContainer>
 	);
