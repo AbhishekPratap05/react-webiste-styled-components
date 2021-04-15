7241b009071deeb3121c4e102a750395c7894493
Abhishek Pratap <35282546+AbhishekPratap05@users.noreply.github.com>
2021-04-15 11:22:02 +0530

    added footer links ✅

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,115 @@
 import React from 'react';
 import { Button } from '../../globalStyle';
+import { FooterLinksItems } from './Footer.elements';
 import {
 	FooterContainer,
+	FooterLink,
+	FooterLinksContainer,
+	FooterLinksWrapper,
+	FooterLinkTile,
 	FooterSubHeading,
 	FooterSubscription,
 	FooterSubText,
 	Form,
 	FormInput
 } from './Footer.elements';
+
+interface FooterNavigationInterface {
+	title: string;
+	links: FooterLinkInterface[];
+}
+
+interface FooterLinkInterface {
+	to: string;
+	text: string;
+}
+
+const footerLinks: FooterNavigationInterface[] = [
+	{
+		title: 'How it works',
+		links: [
+			{
+				to: '/',
+				text: 'Testimonials'
+			},
+			{
+				to: '/',
+				text: 'Careers'
+			},
+			{
+				to: '/',
+				text: 'Investors'
+			},
+			{
+				to: '/',
+				text: 'Terms of Services'
+			}
+		]
+	},
+	{
+		title: 'Contact Us',
+		links: [
+			{
+				to: '/',
+				text: 'Testimonials'
+			},
+			{
+				to: '/',
+				text: 'Careers'
+			},
+			{
+				to: '/',
+				text: 'Investors'
+			},
+			{
+				to: '/',
+				text: 'Terms of Services'
+			}
+		]
+	},
+	{
+		title: 'Videos',
+		links: [
+			{
+				to: '/',
+				text: 'Testimonials'
+			},
+			{
+				to: '/',
+				text: 'Careers'
+			},
+			{
+				to: '/',
+				text: 'Investors'
+			},
+			{
+				to: '/',
+				text: 'Terms of Services'
+			}
+		]
+	},
+	{
+		title: 'Social Media',
+		links: [
+			{
+				to: '/',
+				text: 'Testimonials'
+			},
+			{
+				to: '/',
+				text: 'Careers'
+			},
+			{
+				to: '/',
+				text: 'Investors'
+			},
+			{
+				to: '/',
+				text: 'Terms of Services'
+			}
+		]
+	}
+];
 
 const Footer = () => {
 	return (
@@ -20,6 +122,24 @@ const Footer = () => {
 					<Button fontBig>Subscribe</Button>
 				</Form>
 			</FooterSubscription>
+			<FooterLinksContainer>
+				<FooterLinksWrapper>
+					{footerLinks.map((footerLink: FooterNavigationInterface, index: number) => {
+						return (
+							<FooterLinksItems key={`footerLink${index}`}>
+								<FooterLinkTile>{footerLink.title}</FooterLinkTile>
+								{footerLink.links.map((link: FooterLinkInterface, index: number) => {
+									return (
+										<React.Fragment key={`link${index}`}>
+											<FooterLink to={link.to}>{link.text}</FooterLink>
+										</React.Fragment>
+									);
+								})}
+							</FooterLinksItems>
+						);
+					})}
+				</FooterLinksWrapper>
+			</FooterLinksContainer>
 		</FooterContainer>
 	);
 };
