dd3523e52900b11f20ba9e0ca5c408eebcc73fc9
Abhishek Pratap <35282546+AbhishekPratap05@users.noreply.github.com>
2021-04-15 19:31:54 +0530

    added scroll to top 🔝 after navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import GlobalStyle from './globalStyle';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Footer, Navbar } from './components';
 import Home from './pages/HomePage/Home';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
 	return (
 		<Router>
 			<GlobalStyle />
+			<ScrollToTop />
 			<Navbar />
 			<Switch>
 				<Route path="/" exact component={Home} />
